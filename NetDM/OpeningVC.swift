a35e3711c2a5076bd1960b8cd1e13999b4a23b09
Ryan Dines <ryan.dines@gmail.com>
2016-06-06 15:57:41 -0400

    Persisted username.  Not sure what was last commit, persisted photo data as well

--- NetDM/OpeningVC.swift
+++ NetDM/OpeningVC.swift
@@ -10,7 +10,7 @@ enum UserState{
     case EntryPoint
     // no data to load
     case HasPicture
-    case HasName(userName: String)
+    case HasName
 }
 
 import UIKit
@@ -21,14 +21,9 @@ class OpeningVC: UIViewController, UITextFieldDelegate, UIImagePickerControllerD
     
     let context = appDelegate.managedObjectContext
     var results:[User] = []
+    var user:User!
     var userState = UserState.EntryPoint
     var imagePicker:UIImagePickerController!
-    
-    //var profileImage=UIImage()
-    //var userName:String? = "Ryan"
-    
-    var user:User!
-    let bigFont = UIFont(name: "arial", size: CGFloat(35.0))
     
     @IBOutlet weak var selfieLabel: UILabel!
     
@@ -45,42 +40,18 @@ class OpeningVC: UIViewController, UITextFieldDelegate, UIImagePickerControllerD
     @IBOutlet weak var nameTextField: UITextField!
     
     @IBAction func photoButtonTapped(sender: AnyObject) {
+        
         print("photo tapped")
         
-        // add ability to take photo as well
+// MARK ADD CODE: the ability to take a photo
         
         imagePicker.sourceType = .PhotoLibrary
         
         presentViewController(imagePicker, animated: true, completion: nil)
 
     }
-    
-    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
-
-        let pickedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
-        //user.imageData = UIImagePNGRepresentation(pickedImage)
-
-        dismissViewControllerAnimated(true) {
-            // save the picked data to the VC's property
-            let png = UIImagePNGRepresentation(pickedImage)
-            self.user.imageData = png
-            self.photoButton.imageView?.contentMode = .ScaleAspectFill
-            self.photoButton.setImage(pickedImage, forState: .Normal)
-            //self.determineUserState()
-            self.userState = .HasPicture
-            self.respondToUserState(self.userState)
-        }
-    }
-    
-    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
-        dismissViewControllerAnimated(true, completion: nil)
-    }
-
-    @IBAction func profileButtonTapped(sender: AnyObject) {
-
-    }
     func respondToUserState(userState: UserState){
-// The purpose of this function is to make the view show or hide sub-views based on where the user is in the registration process, remove extraneous stuff to make more modular
+        // The purpose of this function is to make the view show or hide sub-views based on the user
         switch userState{
         case .EntryPoint:
             nameTextField.hidden = true
@@ -102,59 +73,118 @@ class OpeningVC: UIViewController, UITextFieldDelegate, UIImagePickerControllerD
             nameTextField.hidden = true
             nameLabel.hidden = false
             nameLabel.text = user.userName
-            nameLabel.font = bigFont
+            nameLabel.font = UIFont(name: "arial", size: CGFloat(35.0))
             profileButton.hidden = false
             chatButton.hidden = false
             whereNextLabel.hidden = false
-            /*
-            if let validName = userName{
-                //persist data
-                nameLabel.text = validName
-                nameTextField.text = validName
-            }*/
         }
     }
-    func determineUserState(){
-        
-        // make more complex after you figure out pictures
-        if let name = self.user.userName {
-            if name != "John Doe" {
-                userState = .HasName(userName: name)
-            }else{
+    
+    func initializeUser(){
+        do{
+            let request = NSFetchRequest(entityName: "User")
+            print("fetching user")
+            results = try context.executeFetchRequest(request) as! [User]
+        }catch let error as NSError{
+            print("You fucked up \(error.localizedDescription)")
+        }
+        // check to see if there's any saved data, if not make a new one and save.
+        if results.count == 0 {
+            print("No persistent data found")
+            userState = .EntryPoint
+            let entity = NSEntityDescription.entityForName("User", inManagedObjectContext: context)
+            user = User(entity: entity!, insertIntoManagedObjectContext: context)
+            user.userName = "-"
+            print("Instantiated blank name")
+            user.imageData = NSData()
+            print("Instantiated blank data")
+        }else{
+            print("Populating user with persistent data")
+            // if you found some data, use the first one you found to populate user
+            user = results.first
+            print("Setting photoButton image")
+            self.photoButton.imageView?.contentMode = .ScaleAspectFill
+            self.photoButton.setImage(user.profileImage, forState: .Normal)
+            if user.userName == "-" {
                 userState = .HasPicture
+            }else{
+                nameLabel.text = user.userName!
+                userState = .HasName
             }
+
+        }
+        do{
+            print("Saving user")
+            try context.save()
+        }catch let error as NSError{
+            print("Failed to save, error: \(error.localizedDescription)")
+        }
+        respondToUserState(userState)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+
+        let pickedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        //user.imageData = UIImagePNGRepresentation(pickedImage)
+
+        dismissViewControllerAnimated(true) {
+            // save the picked data to the VC's property
+            let png = UIImagePNGRepresentation(pickedImage)
+            self.user.imageData = png
+            self.photoButton.imageView?.contentMode = .ScaleAspectFill
+            self.photoButton.setImage(pickedImage, forState: .Normal)
+            self.userState = .HasPicture
+            self.respondToUserState(self.userState)
         }
     }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    @IBAction func profileButtonTapped(sender: AnyObject) {
+        
+    }
+
     // MARK: UITextFieldDelegate
     
-    func textFieldShouldReturn(textField: UITextField) -> Bool {
-        // Hide the keyboard
-        textField.resignFirstResponder()
-        return true
-    }
-    
     func textFieldDidEndEditing(textField: UITextField) {
-// MARK ADD CODE
-// NEED RIGHT FONT SIZE
+
         if let name = textField.text {
             nameLabel.text = name
             nameLabel.adjustsFontSizeToFitWidth = false
             nameLabel.font = UIFont(name: "arial", size: CGFloat(35.0))
             self.user.userName = name
-            print("adding name")
-            self.userState = .HasName(userName: name)
+            print("added name")
+            self.userState = .HasName
             self.respondToUserState(userState)
         }
     }
     
-/*
-     
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
-        //initializeUser()
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        // Hides the keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    
+// MARK: STANDARD METHODS FOR VC
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Loading view")
+        // title first, catches their eyes probably
+        navigationItem.title = "Net DM"
+        initializeUser()
         // Core Data managed entity "user" should not be nil at this point.
-        respondToUserState(userState)
-    }*/
+        nameTextField.delegate = self
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "segueToConnections" {
             print("Phase 3 Complete, Moving to Connections")
@@ -163,66 +193,6 @@ class OpeningVC: UIViewController, UITextFieldDelegate, UIImagePickerControllerD
             connectionsVC.user = self.user
         }
         super.prepareForSegue(segue, sender: sender)
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        print("Loading view")
-        // title first, catches their eyes probably
-        navigationItem.title = "Net DM"
-        initializeUser()
-        respondToUserState(.EntryPoint)
-        // Core Data managed entity "user" should not be nil at this point.
-        nameTextField.delegate = self
-        imagePicker = UIImagePickerController()
-        imagePicker.delegate = self
-    }
-    
-    func initializeUser(){
-// fetch data from stack
-        do{
-            let request = NSFetchRequest(entityName: "User")
-            results = try context.executeFetchRequest(request) as! [User]
-        }catch let error as NSError{
-            print("You fucked up \(error.localizedDescription)")
-        }
-// check to see if there's any saved data, if not make a new one and save.
-        if results.count == 0 {
-            print("No persistent data found")
-            let entity = NSEntityDescription.entityForName("User", inManagedObjectContext: context)
-            user = User(entity: entity!, insertIntoManagedObjectContext: appDelegate.managedObjectContext)
-            user.userName = "John Doe"
-            print("Instantiated name")
-            user.imageData = NSData()
-            /*
-            if let validImage = UIImage(named: "BlueDialogueBox") {
-                print("Found default picture")
-                user.imageData = NSData(data: UIImageJPEGRepresentation(validImage, 1.0)!)
-            }*/
-            // user.imageData = UIImagePNGRepresentation(UIImage(named: "Background")!)
-            // user.imageData is optional
-           // user.imageData = UIImagePNGRepresentation(self.profileImage)
-            do{
-                try context.save()
-            }catch let error as NSError{
-                print("Failed to save, error: \(error.localizedDescription)")
-            }
-        }else{
-            print("Populating user with persistent data")
-// if you found some data, use the first one you found to populate user
-            user = results.first
-            do{
-                try context.save()
-            }catch let error as NSError {
-                print("Failed to save context: \(error.localizedDescription)")
-            }
-            
-            self.photoButton.imageView?.image = UIImage()
-        }
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
     }
     
 }
--- User.swift
+++ User.swift
@@ -13,91 +13,18 @@ import UIKit
 
 class User: NSManagedObject {
     
-    /*
-    var profileImage:UIImage{
+    var profileImage:UIImage {
+        
         get{
-            let image = UIImage(data: imageData!)!
-            return image
+            print("Converting binary data to photo")
+            // get the image by converting it from binary form of the persistent store
+            return UIImage(data: self.imageData!)!
         }
         set{
-            let image = self.profileImage
-            imageData = UIImagePNGRepresentation(image)
-        }
-    }*/
-    override func awakeFromFetch() {
-        super.awakeFromFetch()
-        print("Fetched stuff")
-        // customization of fetched objects, the ones retrieved from memory
-    }
-    override func awakeFromInsert() {
-        // customization of new objects, used when storing
-        print("Created new stuff")
-    }
-     // compute image from saved data
-    /*
-    var profileImage:UIImage{
-        if self.imageData != nil {
-            if let image = UIImage(data: self.imageData!){
-                return image
-            }else{
-                return UIImage()
-            }
-        }else{
-            return UIImage()
-        }
-    }*/
-    
-    // computed property returns image when needed on run time
-    /*
-    var profileImage:UIImage{
-        let blank = UIImage()
-        if let validData = self.imageData {
-            if let validImage = UIImage(data: validData){
-                print("Made sense of the stored data")
-                return validImage
-            }else{
-                print("Invalid saved image, attempting to load default")
-                if let validImage = UIImage(named: "Background"){
-                    // return default image to avoid crashing while using optionals scenario
-                    print("Attempting to set the profile image to the default")
-                    return validImage
-                }else{
-                    print("Nothing else worked, blank image it is")
-                    // nil case should crash program, for development only
-                    return blank
-                }
-            }
-        }else{
-            print("Two invalid data sets, blank image it is")
-            // nil case should crash program, for development only
-            return blank
-        }
-    }*/
-    func returnProfileImage()->UIImage{
-        let blank = UIImage()
-        if let validData = self.imageData {
-            if let validImage = UIImage(data: validData){
-                print("Made sense of the stored data")
-                return validImage
-            }else{
-                print("Invalid saved image, attempting to load default")
-                if let validImage = UIImage(named: "Background"){
-                    // return default image to avoid crashing while using optionals scenario
-                    print("Attempting to set the profile image to the default")
-                    return validImage
-                }else{
-                    print("Nothing else worked, blank image it is")
-                    // nil case should crash program, for development only
-                    return blank
-                }
-            }
-        }else{
-            print("Two invalid data sets, blank image it is")
-            // nil case should crash program, for development only
-            return blank
+            // set the image by storing its data as an attribute for a Core Data Entity
+            print("Storing image as binary data")
+            self.imageData! = UIImagePNGRepresentation(newValue)!
         }
     }
-    
-    
-    
+
 }
